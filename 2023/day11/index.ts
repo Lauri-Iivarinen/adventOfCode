6e741a4548d1526a2fb7370461783cdc2199749a
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2023-12-11 14:27:28 +0200

    removed unneccessary lines from day 11

--- 2023/day11/index.ts
+++ 2023/day11/index.ts
@@ -7,14 +7,11 @@ interface Galaxy{
 }
 
 //Replace every expansion point with row/col of X
-const expandGalaxyPt2 = (data: string[]) =>{
+const expandGalaxy = (data: string[]) =>{
     let galaxies: string[][] = []
     data.forEach(row => {
-        if (!row.includes('#')) {
-            galaxies.push(row.replaceAll('.', 'X').split(''))
-        } else {
-            galaxies.push(row.split(''))
-        }
+        if (!row.includes('#')) galaxies.push(row.replaceAll('.', 'X').split(''))
+        else galaxies.push(row.split(''))
     })
     
     let galaxyFlipped: string[][] = []
@@ -27,58 +24,14 @@ const expandGalaxyPt2 = (data: string[]) =>{
     }
     galaxies = []
     galaxyFlipped.map(row => row.join().replaceAll(',', '')).forEach(row => {
-        if (!row.includes('#')) {
-            galaxies.push(row.replaceAll('.', 'X').split(''))
-        } else {
-            galaxies.push(row.split(''))
-        }
+        if (!row.includes('#')) galaxies.push(row.replaceAll('.', 'X').split(''))
+        else galaxies.push(row.split(''))
     })
 
     return galaxies
 }
-const expandGalaxy = (data: string[]) => {
-    let galaxies: string[][] = []
-    data.forEach(row => {
-        if (!row.includes('#')) {
-            galaxies.push(row.split(''))
-        }
-        galaxies.push(row.split(''))
-    })
-    
-    let galaxyFlipped: string[][] = []
-    for (let i = 0; i < galaxies[0].length; i++){
-        let flippedRow = []
-        for (let a = 0; a < galaxies.length; a++){
-            flippedRow.push(galaxies[a][i])
-        }
-        galaxyFlipped.push(flippedRow)
-    }
-    galaxies = []
-    galaxyFlipped.map(row => row.join().replaceAll(',', '')).forEach(row => {
-        if (!row.includes('#')) {
-            galaxies.push(row.split(''))
-        }
-        galaxies.push(row.split(''))
-    })
 
-    return galaxies
-    
-}
-
-const calcDist = ( galaxyPairs: [Galaxy, Galaxy][]) => {
-    let sum = 0
-    //calc distance
-    galaxyPairs.forEach(pair => {
-        let distY = pair[1].coords[0] - pair[0].coords[0]
-        let distX = pair[1].coords[1] > pair[0].coords[1] ? pair[1].coords[1] - pair[0].coords[1] : pair[0].coords[1] - pair[1].coords[1]
-
-        sum+= distX+distY
-    })
-    console.log('pt1: ', sum)
-}
-
-const calcDistPt2 = (galaxyMap: string[][], galaxyPairs: [Galaxy, Galaxy][]) => {
-    const expansionDist = 1000000
+const calcDist = (galaxyMap: string[][], galaxyPairs: [Galaxy, Galaxy][], expansionDist: number) => {
     let sum = 0
     //calc distance
     galaxyPairs.forEach(pair => {
@@ -87,40 +40,32 @@ const calcDistPt2 = (galaxyMap: string[][], galaxyPairs: [Galaxy, Galaxy][]) => 
         let distX = pair[1].coords[1] > pair[0].coords[1] ? pair[1].coords[1] - pair[0].coords[1] : pair[0].coords[1] - pair[1].coords[1]
         //Find out how many expanded sections are crossed on both axis
         for (let i = pair[0].coords[0]; i < pair[0].coords[0] + distY; i++){
-            if (galaxyMap[i][0] === 'X') {
-                expansions++
-            }
+            if (galaxyMap[i][0] === 'X') expansions++
         }
         let startX = pair[1].coords[1] > pair[0].coords[1] ? pair[0].coords[1] : pair[1].coords[1]
         for (let i = startX; i <= distX+startX; i++){
-            if (galaxyMap[0][i] === 'X') {
-                expansions++
-            }
+            if (galaxyMap[0][i] === 'X') expansions++
         }
 
         sum += distX + distY + (expansions * expansionDist) - expansions
     })
-    console.log('pt2: ', sum)
+
+    console.log('result: ', sum)
 }
 
 const main = (data: string[]) => {
     //Expand universe, find closest distance for each galaxy pair
-    //const galaxies = expandGalaxy(data) // <-- For part 1
-    const galaxies = expandGalaxyPt2(data)
+    const galaxies = expandGalaxy(data)
     //galaxies.forEach(row => console.log(row.join().replaceAll(',', ' ')))
+
     const galaxyInd: [number,number][] = []
     galaxies.forEach((row, index) => {
         row.forEach((char, charIndex) => {
             if (char === '#') galaxyInd.push([index, charIndex])
         })
     })
-    const galaxyMap: Galaxy[] = galaxyInd.map((coords, i) => {
-        return {
-            code: i + 1,
-            coords
-        }
-    })
-    //console.log(galaxyMap)
+    const galaxyMap: Galaxy[] = galaxyInd.map((coords, i) => { return {code: i + 1,coords}})
+
     const galaxyPairs: [Galaxy, Galaxy][] = []
     let startIndex = 0
     while (startIndex < galaxyMap.length) {
@@ -129,8 +74,9 @@ const main = (data: string[]) => {
         }
         startIndex++
     }
-    //calcDist(galaxyPairs)
-    calcDistPt2(galaxies, galaxyPairs)
+
+    calcDist(galaxies, galaxyPairs, 2) //pt 1
+    calcDist(galaxies, galaxyPairs, 1000000) //pt 2
 }
 
 readFile(`day${day}/input.txt`, "utf8", (err, data: string) => {
