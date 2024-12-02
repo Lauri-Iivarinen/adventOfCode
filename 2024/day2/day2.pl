a75b98d11f06e051eae5abfc60c4eeeb138843a3
Lauri <lauri.iivarinen@gmail.com>
2024-12-02 18:53:02 +0200

    d2 pt1

---
+++ 2024/day2/day2.pl
@@ -0,0 +1,31 @@
+open(FH, '<', 'input.txt') or die $!;
+
+my $count = 0;
+
+while (<FH>){
+    chomp;
+    my @report = split / /, $_;
+    
+    my $increasing = $report[0] < $report[1];
+    my $len = @report;
+    my $safe = 1;
+    for (my $i = 1; $i<$len; $i++){
+        if (($increasing && $report[$i-1] >= $report[$i])){
+            $safe = 0;
+        }elsif($increasing && $report[$i]-$report[$i-1]>3) {
+            $safe = 0;
+        }elsif ((!$increasing && $report[$i] >= $report[$i-1])){
+            $safe = 0;
+        }elsif(!$increasing && $report[$i-1]-$report[$i]>3){
+            $safe = 0;
+        }
+    }
+    
+    if ($safe){
+        $count++;
+    }
+}
+
+close FH;
+
+print "COUNT: $count\n";
