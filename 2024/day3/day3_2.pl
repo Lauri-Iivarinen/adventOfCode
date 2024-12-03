70ae372b35f7d61db29b43a8cb4abf512c414374
Lauri <lauri.iivarinen@gmail.com>
2024-12-04 00:29:29 +0200

    pt2 in 8 lines as well:D

--- 2024/day3/day3_2.pl
+++ 2024/day3/day3_2.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 open(FH, '<', 'input.txt') or die $!;
 my $sum = 0;
-for my $i ((join "" , <FH>) =~ m/(mul\([0-9]+,[0-9]+\))/g) {
+for my $i ((join "" , <FH>) =~ s/\R//rg =~ s/don\'t\(\).*?do\(\)//rg =~ m/(mul\([0-9]+,[0-9]+\))/g) {
     my @a = $i =~ m/[0-9]+/g;
     $sum += $a[0]*$a[1];
 }
